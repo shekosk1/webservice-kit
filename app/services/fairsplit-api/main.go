e78938c2b31bf593440252ee0ef6e10bc8243ef5
shekosk1 <nicolas.pacheco.dev@gmail.com>
2023-04-30 23:58:17 -0400

    add: maxprocs

--- app/services/fairsplit-api/main.go
+++ app/services/fairsplit-api/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 
 	"github.com/shekosk1/webservice-kit/foundation/logger"
+	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 )
 
@@ -32,6 +33,10 @@ func run(log *zap.SugaredLogger) error {
 	/*
 		GOMAXPROCS
 	*/
+	opt := maxprocs.Logger(log.Infof)
+	if _, err := maxprocs.Set(opt); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
 	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
 
 	defer log.Infow("shutdown")
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/shekosk1/webservice-kit
 
 go 1.19
 
-require go.uber.org/zap v1.24.0
+require (
+	go.uber.org/automaxprocs v1.5.2
+	go.uber.org/zap v1.24.0
+)
 
 require (
 	go.uber.org/atomic v1.7.0 // indirect
